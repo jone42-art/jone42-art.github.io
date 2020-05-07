22ed2a6655c46a06125f5ad7617b35058ede2b78
jone42-art <64969171+jone42-art@users.noreply.github.com>
2020-05-07 16:00:15 +0100

    uploading website

---
+++ script.js
@@ -0,0 +1,14 @@
+var todos = [
+  "clean room",
+  "brush teeth",
+  "exercise",
+  "study javascript",
+  "eat healthy"
+  ];
+
+
+
+  for (var i=0; i < todos.lenght; i++ ) {
+    console.log(i);
+  }
+  system.out.print("God is Good");
